ad6d73c06464ac94e7c39e9aff0cdbe95bb327a8
FeverCode <giddylancs.gl@gmail.com>
2022-03-13 15:11:41 +0300

    git commit will add jquery for showing hidden description on click for what we do section

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,26 @@
-$(document).ready(function(event){
-    event.preventDefault
+
+// user interface logic
+$(document).ready(function(){
+    $("#design").click(function() {
+        $(".design").toggle();
+    });
+
 
 });
+
+$(document).ready(function(){
+    $("#development").click(function() {
+        $(".development").toggle();
+    });
+
+
+});
+
+$(document).ready(function(){
+    $("#management").click(function() {
+        $(".management").toggle();
+    });
+
+
+});
+
